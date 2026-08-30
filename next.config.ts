2105eb0ba414bce49874630971d3855ad086e7df
Rafael da Silva Bitencourt <rafaelsbiten@hotmail.com>
2026-08-30 11:41:35 -0300

    feat: add GitHub Pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: "/nextjs-with-auth-client-only",
 };
 
 export default nextConfig;
