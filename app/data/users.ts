8d51ee9d3e0233a370e091309aa61ab2f0836be6
Rafael da Silva Bitencourt <rafaelsbiten@hotmail.com>
2026-08-30 09:26:13 -0300

    feat: scaffold project

---
+++ app/data/users.ts
@@ -0,0 +1,16 @@
+const users = [
+  {
+    id: "1",
+    name: "John Doe",
+    email: "admin@example.com",
+    password: "password123",
+    role: "admin",
+  },
+  {
+    id: "2",
+    name: "Jane Doe",
+    email: "user@example.com",
+    password: "password123",
+    role: "user",
+  },
+];
